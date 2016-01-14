730f1d059afe2b4b34df7b449fd10e4b065d465c
Kornel Lugosi <coornail@gmail.com>
2016-01-14 14:32:13 +0100

    Temporary fix for an incompatible function chaining

--- react-chosen.js
+++ react-chosen.js
@@ -17,25 +17,25 @@
     componentDidMount: function() {
       var props = this.props;
       var select = $(this.refs.select.getDOMNode());
-      $(select)
-        .chosen({
-          allow_single_deselect: props.allowSingleDeselect,
-          disable_search: props.disableSearch,
-          disable_search_threshold: props.disableSearchThreshold,
-          enable_split_word_search: props.enableSplitWordSearch,
-          inherit_select_classes: props.inheritSelectClasses,
-          max_selected_options: props.maxSelectedOptions,
-          no_results_text: props.noResultsText,
-          placeholder_text_multiple: props.placeholderTextMultiple,
-          placeholder_text_single: props.placeholderTextSingle,
-          search_contains: props.searchContains,
-          single_backstroke_delete: props.singleBackstrokeDelete,
-          width: props.width,
-          display_disabled_options: props.displayDisabledOptions,
-          display_selected_options: props.displaySelectedOptions
-        })
-        .on('chosen:maxselected', this.props.onMaxSelected)
-        .change(this.handleChange);
+      var $select = $(select);
+      $select.chosen({
+        allow_single_deselect: props.allowSingleDeselect,
+        disable_search: props.disableSearch,
+        disable_search_threshold: props.disableSearchThreshold,
+        enable_split_word_search: props.enableSplitWordSearch,
+        inherit_select_classes: props.inheritSelectClasses,
+        max_selected_options: props.maxSelectedOptions,
+        no_results_text: props.noResultsText,
+        placeholder_text_multiple: props.placeholderTextMultiple,
+        placeholder_text_single: props.placeholderTextSingle,
+        search_contains: props.searchContains,
+        single_backstroke_delete: props.singleBackstrokeDelete,
+        width: props.width,
+        display_disabled_options: props.displayDisabledOptions,
+        display_selected_options: props.displaySelectedOptions
+      });
+      $select.on('chosen:maxselected', this.props.onMaxSelected);
+      $select.change(this.handleChange);
     },
 
     componentWillUnmount: function() {
